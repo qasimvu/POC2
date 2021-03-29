fa6a0b3fa96131afc873b3a21a3a8a5f0eec0a17
qasimvu <qalam@enquizit.com>
2021-03-29 16:38:36 +0500

    Removed Owner Last Name

--- src/test/java/com/ccaoa/Base/Base.java
+++ src/test/java/com/ccaoa/Base/Base.java
@@ -13,7 +13,7 @@ import org.testng.annotations.*;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-// Added by Qasim Alam
+// Added by Qasim
 
 public class Base {
     public static WebDriver driver = null;
